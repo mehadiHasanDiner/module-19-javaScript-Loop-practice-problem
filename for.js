5e87a8c9da0c48bbce63f32a6f58e4a9bdcc655c
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-28 03:08:52 +0600

    for loop

--- for.js
+++ for.js
@@ -1,14 +1,14 @@
 // 0 to 10
-for (i = 0; i <= 10; i++) {
+for (var i = 0; i <= 10; i++) {
   console.log(i);
 }
 
 // 0 to 10 even numbers
-for (i = 0; i <= 10; i = +2) {
+for (var i = 0; i <= 10; i = +2) {
   console.log("even number " + i);
 }
 
 // 1 to 10 odd numbers
-for (i = 1; i <= 10; i = +2) {
+for (var i = 1; i <= 10; i = +2) {
   console.log("odd number " + i);
 }
