e95462dae080798a2c9f4bae8e8048fd21055370
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-28 16:55:48 +0600

    module done

---
+++ reverse.js
@@ -0,0 +1,15 @@
+// while loop in a reverse way
+// var number = 10;
+// while (number >= 1) {
+//   console.log(number);
+//   number--;
+// }
+
+// for loop in a reverse way
+// for (var i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// var i = 0;
+// for (i = 0; i < 5; i++) {}
+// console.log(i);
