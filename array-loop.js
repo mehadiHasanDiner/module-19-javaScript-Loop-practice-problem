09b5661757b837a08e55eaf67584181e9a57ba16
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-01 00:41:57 +0600

    array-for loop updated

--- array-loop.js
+++ array-loop.js
@@ -1,13 +1,25 @@
-// targe: display every elements of an array
+// // targe: display every elements of an array
 
-var numbers = [45, 64, 23, 78, 90, 47, 12, 100, 45];
+// var numbers = [45, 64, 23, 78, 90, 47, 12, 100, 45];
 
-for (var i = 0; i < numbers.length; i++) {
-  var number = numbers[i];
-  console.log(number);
-}
+// for (var i = 0; i < numbers.length; i++) {
+//   var number = numbers[i];
+//   console.log(number);
+// }
 
-var items = ["bottle", "mouse", "sunglass", "pen", "notebook"];
+// targe: display every elements of an array of strings
+
+var items = [
+  "bottle",
+  "mouse",
+  "sunglass",
+  "pen",
+  "notebook",
+  "book",
+  "pencil",
+  "eraser",
+  "pencil-cutter",
+];
 
 for (var i = 0; i < items.length; i++) {
   var item = items[i];
