6973acad56e72d2096a061117c563b37c985b7bf
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-06-30 20:27:26 +0600

    revision.js updated

--- revision.js
+++ revision.js
@@ -1,3 +1,14 @@
+// string type variable
+var bottle = "water";
+var bottleColor = "yellow";
+
+// number type variable
+var sunglassPrice = 120;
+var penColor = "blue";
+
+// boolean type variable
+var isExpensive = true;
+
 var tableItems = ["bottle", "pen", "mouse", "sunglass", "paper"];
 
 var item3 = tableItems[3];
