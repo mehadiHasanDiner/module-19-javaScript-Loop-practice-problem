212771b9e7e931afa4245e382e121e86b4eb7f5d
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-01 02:07:53 +0600

    break in JS

--- break.js
+++ break.js
@@ -1,38 +1,49 @@
-// for loop break
-// for (var i = 1; i <= 20; i++) {
+// // for loop break
+// for (var i = 0; i <= 20; i++) {
+//   console.log(i);
 //   if (i > 10) {
 //     break;
 //   }
-//   console.log(i);
 // }
 
-// while loop break
-// var roastGive = 0;
-// while (roastGive < 7) {
-//   console.log("Roast den, git item ansi");
-//   roastGive++;
-//   if (roastGive > 4) {
+// // while loop break
+// var roastGiven = 0;
+// while (roastGiven < 10) {
+//   console.log("roast den, gift item ansi");
+//   roastGiven++;
+//   if (roastGiven > 4) {
 //     break;
 //   }
 // }
 
-// var items = ["bottle", "mouse", "sunglass", "pen", "notebook"];
+// //  break in string array
+// var items = [
+//   "bottle",
+//   "mouse",
+//   "sunglass",
+//   "pen",
+//   "notebook",
+//   "book",
+//   "pencil",
+//   "eraser",
+//   "pencil-cutter",
+// ];
 
 // for (var i = 0; i < items.length; i++) {
 //   var item = items[i];
-//   if (item == "pen") {
+//   if (items[i] == "pencil") {
 //     break;
 //   }
 //   console.log(item);
 // }
 
 // Continue
-var numbers = [45, 64, 23, 78, 90, 47, 12, 100, 45];
+var numbers = [45, 64, 23, 78, 10, 47, 112, 90, 45];
 
 for (var i = 0; i < numbers.length; i++) {
   var number = numbers[i];
-  if (number > 80) {
-    continue;
+  if (number >= 100) {
+    break;
   }
   console.log(number);
 }
