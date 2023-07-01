b1676847501639d52e6bd4551fefa873b849af6d
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-01 10:43:32 +0600

    reverse.js

--- reverse.js
+++ reverse.js
@@ -1,14 +1,26 @@
 // while loop in a reverse way
+var num = 100;
+while (num >= 90) {
+  //   console.log(num);
+  num--;
+}
+
+// while loop in a reverse way
 // var number = 10;
 // while (number >= 1) {
 //   console.log(number);
 //   number--;
 // }
 
-// for loop in a reverse way
-// for (var i = 10; i >= 1; i--) {
-//   console.log(i);
-// }
+// for loop
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// for loop in a reverse way (100-80)
+for (var i = 100; i >= 80; i--) {
+  console.log(i);
+}
 
 // var i = 0;
 // for (i = 0; i < 5; i++) {}
