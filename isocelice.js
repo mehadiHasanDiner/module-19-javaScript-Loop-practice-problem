ca588aec4e75863472cfb07231d0d2a534fc7f24
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-01 20:05:42 +0600

    isocelice.js

---
+++ isocelice.js
@@ -0,0 +1,9 @@
+var side1 = 9;
+var side2 = 10;
+var side3 = 23;
+
+if (side1 == side2 || side1 == side3 || side2 == side3) {
+  console.log("isosceles");
+} else {
+  console.log(" Not isosceles");
+}
