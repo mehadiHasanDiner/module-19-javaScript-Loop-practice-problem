3f41cd18c6439dee5785f4fc26635b10f3a82efd
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-01 11:41:37 +0600

    array repeat

---
+++ array.js
@@ -0,0 +1,16 @@
+var fruits = ["apple", "watermelon", "banana", "mango", "pineapple", "orange"];
+
+var indexofBanana = fruits.indexOf("banana");
+console.log(indexofBanana);
+
+// var totalElement = fruits.length;
+// console.log(totalElement);
+
+fruits[2] = "Mango";
+
+fruits.pop();
+fruits.push("Lemon");
+console.log(fruits);
+
+// var indexOfOrange = fruits.indexOf("orange");
+// console.log(indexOfOrange);
