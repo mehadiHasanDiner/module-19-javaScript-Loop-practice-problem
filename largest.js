f0d9fe8d5dfbd1d7b17e04759659be89b4ec0091
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-01 12:30:41 +0600

    largest in 3 number

---
+++ largest.js
@@ -0,0 +1,17 @@
+var num1 = 400;
+var num2 = 6969696;
+var num3 = 999999;
+
+if (num1 > num2) {
+  if (num1 > num3) {
+    console.log("number 1 is big");
+  } else {
+    console.log("number 3 is big");
+  }
+} else {
+  if (num2 > num3) {
+    console.log("number 2 is big");
+  } else {
+    console.log("number 3 is big");
+  }
+}
